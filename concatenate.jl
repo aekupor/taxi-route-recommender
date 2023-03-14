9ecba66eb615e4f26aae49285d0b52af6c25e10a
Ashlee Kupor <akupor@DN525f77.SUNet>
2023-03-13 22:55:07 -0700

    Make and concatendate test files

--- concatenate.jl
+++ concatenate.jl
@@ -6,10 +6,10 @@ df = DataFrame()
 
 # loop over each file and append its contents to the DataFrame
 for i in 1:16
-    filename = "train_dataset_riya_$i.txt"
+    filename = "test_dataset_ashlee_$i.txt"
     temp_df = CSV.read(filename, header=true, delim='\t', DataFrame)
     global df = vcat(df, temp_df)
 end
 
 # write the concatenated DataFrame to a new file
-CSV.write("concatenated_train_files.txt", df)
+CSV.write("concatenated_test_files.txt", df)
